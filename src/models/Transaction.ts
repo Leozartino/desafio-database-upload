305c160241b8af1de63e25591596d9a12ae368a5
Leozartino <leozartino@gmail.com>
2020-05-05 12:09:34 -0300

    feat(transaction.ts): Cria o model Transaction

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,40 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('numeric')
   value: number;
 
+  @JoinColumn({ name: 'category_id' })
+  @ManyToOne(() => Category, category => category.transactions)
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
